f89ca61de6d0a6fe0b232ce352acf08c2707a441
devarchi33 <devarchi33@gmail.com>
2016-07-03 23:21:32 +0900

    1. 홈 화면 card 수정.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -6,8 +6,14 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
     private slides;
+    private items;
 
     constructor(private navController:NavController) {
+        this.initializeSlides();
+        this.initializeItems();
+    }
+
+    initializeSlides() {
         this.slides = [
             {
                 title: "Welcome to the Docs!",
@@ -25,5 +31,62 @@ export class HomePage {
                 image: "images/thumbnail-duckling-3.jpg",
             }
         ];
+    };
+
+    initializeItems() {
+        this.items = [
+            'Amsterdam',
+            'Bogota',
+            'Buenos Aires',
+            'Cairo',
+            'Dhaka',
+            'Edinburgh',
+            'Geneva',
+            'Genoa',
+            'Glasglow',
+            'Hanoi',
+            'Hong Kong',
+            'Islamabad',
+            'Istanbul',
+            'Jakarta',
+            'Kiel',
+            'Kyoto',
+            'Le Havre',
+            'Lebanon',
+            'Lhasa',
+            'Lima',
+            'London',
+            'Los Angeles',
+            'Madrid',
+            'Manila',
+            'New York',
+            'Olympia',
+            'Oslo',
+            'Panama City',
+            'Peking',
+            'Philadelphia',
+            'San Francisco',
+            'Seoul',
+            'Taipeh',
+            'Tel Aviv',
+            'Tokio',
+            'Uelzen',
+            'Washington'
+        ];
+    }
+
+    getItems(ev) {
+        // Reset items back to all of the items
+        this.initializeItems();
+
+        // set val to the value of the ev target
+        var val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.items = this.items.filter((item) => {
+                return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+        }
     }
 }
