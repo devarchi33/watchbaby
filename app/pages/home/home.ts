9d9ab8ac16ea2c255bd7b2b4d870d35740cddc45
devarchi33 <devarchi33@gmail.com>
2016-07-04 20:59:31 +0900

    1. 마이 페이지 click navigator 추가.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -3,13 +3,18 @@ import {NavController} from 'ionic-angular';
 import {MainSearchPage} from './search/main-search';
 import {MainSlidePage} from './slides/main-slide';
 import {MainCardPage} from './cards/main-card';
+import {MyPage} from "../my-page/my-page";
 
 @Component({
     directives: [MainSearchPage, MainSlidePage, MainCardPage],
     templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-    constructor(private navController:NavController) {
 
+    constructor(private navController:NavController) {
     };
+
+    moveMyPage() {
+        this.navController.push(MyPage);
+    }
 }
