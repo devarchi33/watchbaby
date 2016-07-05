a809b324b380a5ef708bcf58890288f136d52c4a
skyfly33 <imfly7@naver.com>
2016-07-05 18:10:57 +0900

    1. header menu btn 기능 완성.

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,42 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomeTabMenu} from './pages/home/tabs/tabs';
+import {LoginPage} from './pages/login/login';
+import {SignupPage} from './pages/signup/signup';
 
 @Component({
-    template: '<ion-nav [root]="rootPage"></ion-nav>'
+    templateUrl: 'build/app.html'
 })
 export class MyApp {
-    rootPage:any = HomeTabMenu;
+    private rootPage:any = HomeTabMenu;
+    private loginPage = LoginPage;
+    private signupPage = SignupPage;
 
-    constructor(platform:Platform) {
+
+    constructor(private platform:Platform,
+                private menu:MenuController) {
+
+        menu.enable(true);
+
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             StatusBar.styleDefault();
         });
     }
+
+    openPage(page) {
+        console.log("clicked!" + page);
+
+        // Reset the nav controller to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.rootPage = page;
+
+        // close the menu when clicking a link from the menu
+        this.menu.close();
+    }
 }
+
 
 ionicBootstrap(MyApp);
