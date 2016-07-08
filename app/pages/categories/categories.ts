221ead1f81d84fe3384064779f360189c8a11a30
skyfly33 <imfly7@naver.com>
2016-07-08 15:18:52 +0900

    1. 카테고리에 카드 추가.

--- app/pages/categories/categories.ts
+++ app/pages/categories/categories.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {HeaderPage} from '../commons/header/header';
+import {CardToothpastePage} from '../home/cards/toothpaste/card-toothpaste';
+import {CardBabyPowderPage} from '../home/cards/baby-powder/card-baby-powder';
 
 /*
  Generated class for the CategoriesPage page.
@@ -9,7 +11,7 @@ import {HeaderPage} from '../commons/header/header';
  Ionic pages and navigation.
  */
 @Component({
-    directives: [HeaderPage],
+    directives: [HeaderPage, CardToothpastePage, CardBabyPowderPage],
     templateUrl: 'build/pages/categories/categories.html',
 })
 export class CategoriesPage {
