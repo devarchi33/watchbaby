8b59971a28a4f586e08b8bc2f965f10fd66dac4e
skyfly33 <imfly7@naver.com>
2016-07-08 17:53:18 +0900

    1. 베이비 파우더 card 구조화 (미완성.)

--- app/pages/home/cards/baby-powder/card-baby-powder.ts
+++ app/pages/home/cards/baby-powder/card-baby-powder.ts
@@ -9,7 +9,67 @@ import {NavController} from 'ionic-angular';
     templateUrl: "build/pages/home/cards/baby-powder/card-baby-powder.html"
 })
 export class CardBabyPowderPage {
-    constructor(private navController:NavController) {
 
+    private cardsL;
+    private cardsR;
+
+    constructor(private navController:NavController) {
+        this.initializeCard();
+    }
+
+    initializeCard() {
+        this.cardsL = [
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            }
+        ];
+
+        this.cardsR = [
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            },
+            {
+                title: "Marty McFly",
+                date: "November 5, 1955",
+                image: "images/advance-card-bttf.png",
+                description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...out of aDeLorean?! Whoa. This is heavy."
+            }
+        ];
     }
 }
